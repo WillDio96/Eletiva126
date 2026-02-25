8ce3b9ba11626e2d293125c51cc8a20770814b08
William <will.dio.wd@gmail.com>
2026-02-24 21:37:56 -0300

    Fiz o exercício 1

--- Lista de exercicios/ex1.php
+++ Lista de exercicios/ex1.php
@@ -28,8 +28,7 @@
                 $valor2 = $_POST['valor2'];
                 $soma = $valor1 + $valor2;
                 echo "A soma é: $soma";
-            }
-        
+            }        
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
